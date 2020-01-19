aa25d5a35fb3268c135206358118c07e5ed84702
Tim St.Clair <tim.stclair@gmail.com>
2020-01-19 23:15:50 +1100

    bold hint

--- src/plugins/upload/plugin.php
+++ src/plugins/upload/plugin.php
@@ -4,7 +4,7 @@
     <fieldset class="uk-fieldset">
         <div class="uk-form-custom">
           <input id="localUpload" type="file">
-          <button class="uk-button uk-button-default" type="button" tabindex="-1">Select mp3, mp4, m3u8, ogg to upload</button>
+          <button class="uk-button uk-button-default" type="button" tabindex="-1">Select mp3, <b>mp4</b>, m3u8, ogg to upload</button>
         </div>
     </fieldset>
   </div>
